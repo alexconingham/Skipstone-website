70f973600302768d2ce1094427adfcc5cc2f7ee6
alexconingham <alex.coningham@gmail.com>
2025-06-14 09:22:10 +1200

    Fix deployment issues: add missing dice_data.json and remove deprecated appDir config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['skipstone.co.nz', 'www.skipstone.co.nz'],
     dangerouslyAllowSVG: true,
