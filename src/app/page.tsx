651a6d805cbc1b717b74cf40afceff6189699258
alexconingham <alex.coningham@gmail.com>
2025-06-14 18:00:52 +1200

    Remove bouncing scroll arrow to prevent component overlap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,12 +90,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce z-10">
-          <svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </div>
+
       </section>
 
       {/* Dungeons Section */}
